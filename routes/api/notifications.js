b7051b89bd2fa2a9e64a8ecfdafef58d8d4d4278
Sabarish <k.sabarish@milvik.se>
2019-09-12 13:02:10 +0530

    Implementing Proxy and smaple codes for proxy

--- routes/api/notifications.js
+++ routes/api/notifications.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
 const auth = require('../../middleware/auth');
 
 // @route    POST api/notifications
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const path = require('path');
-
+const config = require('config');
+const proxy = require('express-http-proxy');
 const app = express();
 
 // Middleware
@@ -8,7 +8,24 @@ app.use(express.json({ extended: false }));
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/proxy', require('./routes/api/proxy'));
 app.use('/api/notifications', require('./routes/api/notifications'));
+
+
+
+let items = ['prev','last','first','next'];
+var randomItem = items[Math.floor(Math.random()*items.length)];
+// let 
+// //Proxy Setup 
+app.use('/proxy', proxy(`https://api.github.com/users/bradtraversy/repos?per_page=1&sort=created:asc&client_id=${
+    config.get('githubClientId' )
+}&client_secret=${
+    config.get('githubSecret')}`, {
+        userResDecorator: function(proxyRes, proxyResData, userReq, userRes) {
+            console.log(proxyResData.length);
+            return proxyResData;
+          }
+}));
 
 const PORT = process.env.PORT || 5000;
 
